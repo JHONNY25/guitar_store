ed9ac791ba9846acea578120cc3b2ab7465271e3
Jonathan Rodríguez <jonas97isk@hotmail.com>
2025-07-16 22:54:40 -0700

    add to cart, increase, decrease

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,60 @@ import  {db} from './Database/db'
 
 function App() {
   const [database, setDatabase] = useState(db)
+  const [cart, setCart] = useState([])
+  const MIN_ITEMS = 1
+
+  function addToCart(item){
+    const itemExist = cart.findIndex(guitar => guitar.id === item.id)
+
+    if(itemExist >= 0){ //exist in the cart
+        const updatedCart = [...cart]
+        updatedCart[itemExist].quantity++
+        setCart(updatedCart)
+    }else{
+        item.quantity = 1
+        setCart([...cart, item])
+    }
+  }
+
+  function removeFromCart(id){
+    //solo deja los articulos diferentes al que elimine
+    setCart(prevCart => prevCart.filter(guitar => guitar.id !== id))
+  }
+
+  function cartIncrement(id){
+    const updatedCart = cart.map(item => {
+        if(item.id === id){
+            return {
+                ...item,
+                quantity: item.quantity + 1
+            }
+        }
+        return item
+    })
+    setCart(updatedCart)
+  }
+
+  function cartDecrement(id){
+    const updatedCart = cart.map(item => {
+        if(item.id === id && item.quantity > MIN_ITEMS){
+            return {
+                ...item,
+                quantity: item.quantity - 1
+            }
+        }
+        return item
+    })
+    setCart(updatedCart)
+  }
+
+  function cleanCart(){ 
+    setCart([])
+  }
 
   return (
     <>
-        <Header/>
+        <Header cart={cart} removeFromCart={removeFromCart} cartIncrement={cartIncrement} cartDecrement={cartDecrement} cleanCart={cleanCart} />
 
         <main className="container-xl mt-5">
             <h2 className="text-center">Nuestra Colección</h2>
@@ -19,6 +69,7 @@ function App() {
                     database.map((guitar) =>(
                         <Collection key={ guitar.id }
                             guitar={guitar}
+                            addToCart = {addToCart}
                         />
                     ))
                 }
--- src/Components/Collection.jsx
+++ src/Components/Collection.jsx
@@ -1,5 +1,5 @@
 
-export default function Collection({guitar}){
+export default function Collection({guitar,addToCart}){
 
     const {id, name, description, price, image} = guitar
 
@@ -15,6 +15,7 @@ export default function Collection({guitar}){
                 <button 
                     type="button"
                     className="btn btn-dark w-100"
+                    onClick={() => addToCart(guitar)}
                 >Agregar al Carrito</button>
             </div>
         </div>
